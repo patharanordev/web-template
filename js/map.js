9632ab11e650bf2b6fef6eb1de97736d1870aab2
patharanordev <patharanor.dev@gmail.com>
2016-05-06 23:03:12 +0700

    Update map.js

--- js/map.js
+++ js/map.js
@@ -7,15 +7,19 @@
       zoom: 12
   });
 
-  var map_bg = L.map('map', {
+  var map = L.map('map', {
       zoomControl: false,
       center: [39.583,2.670],
       zoom: 12
   });
 
-  var basemap = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+  var basemap_bg = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
   }).addTo(map_bg);
+
+  var basemap_phone = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+  }).addTo(map);
 
   //Layers definition
   // var layers = {
@@ -27,12 +31,12 @@
 
 
   // Empty layer
-  cartodb.createLayer(map_bg,{
+  cartodb.createLayer(map,{
       user_name: 'patharanor',
       type: 'cartodb',
       sublayers: []
-    }, 'https://patharanor.cartodb.com/api/v2/viz/adf5f1f0-12fc-11e6-8132-0e787de82d45/viz.json')
-    .addTo(map_bg)
+    }, 'https://patharanor.cartodb.com/api/v2/viz/869f8dec-1346-11e6-9a97-0e787de82d45/viz.json')
+    .addTo(map)
     .done(function(layer){
       // When the layers inputs change fire this
       // $("input[name='layer']").change(function(){
@@ -45,14 +49,17 @@
       //       layer.createSubLayer(layers[$(this).attr("id")]);
       //   });
       // });
+
+
+    
   });
 
-  cartodb.createLayer(map,{
+  cartodb.createLayer(map_bg,{
       user_name: 'patharanor',
       type: 'cartodb',
       sublayers: []
     }, 'https://patharanor.cartodb.com/api/v2/viz/adf5f1f0-12fc-11e6-8132-0e787de82d45/viz.json')
-    .addTo(map)
+    .addTo(map_bg)
     .done(function(layer){
       // When the layers inputs change fire this
       // $("input[name='layer']").change(function(){
